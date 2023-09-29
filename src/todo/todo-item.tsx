d649289c5924fc6bd9875f95b5e6457b643695ea
vitorthedev <netd777@gmail.com>
2023-09-29 16:46:18 -0300

    style todo-item

--- src/todo/todo-item.tsx
+++ src/todo/todo-item.tsx
@@ -2,9 +2,9 @@ import { TodoItemModel } from './todo-item-model';
 
 function TodoItem({ item, onDelete }: { item: TodoItemModel, onDelete: (todo: TodoItemModel) => void, }) {
 
-  return (<li>
-    {item.description}
-    <button onClick={() => onDelete(item)}>X</button>
+  return (<li className="min-w-[15rem] flex flex-row justify-between m-2">
+    <div>{item.description}</div>
+    <button className='font-bold text-red-600 p-1' onClick={() => onDelete(item)}>X</button>
   </li>)
 }
 
