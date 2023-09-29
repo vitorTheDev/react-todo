9910aca0920530f42b653b658e5c5cba93cf125a
vitorthedev <netd777@gmail.com>
2023-09-29 17:27:19 -0300

    enhance respositivity, improve styling

--- src/todo/todo-form.tsx
+++ src/todo/todo-form.tsx
@@ -10,13 +10,14 @@ function TodoForm({ onAdd }: { onAdd: (todo: TodoItemModel) => void}) {
     e?.preventDefault();
     if (description) {
       onAdd?.({description})
+      setDescription('')
     }
   }
 
   return (<form onSubmit={handleSubmit}>
-    <div className="min-w-[15rem] flex flex-row justify-between gap-3 m-2">
-      <input className="border-2 rounded-md" onChange={descriptionChanged}></input>
-      <button className="p-1 shadow-md active:shadow-lg border-2 rounded-md" type="submit">Adicionar</button>
+    <div className="min-w-[15rem] max-w-2xl w-[100dvw] flex flex-row justify-between gap-4 py-4 px-6">
+      <input className="border-2 rounded-md w-full" onChange={descriptionChanged} value={description}></input>
+      <button className="p-2 shadow-md active:shadow-lg border-2 rounded-md" type="submit">Adicionar</button>
     </div>
   </form>)
 }
--- src/todo/todo-item.tsx
+++ src/todo/todo-item.tsx
@@ -2,9 +2,9 @@ import { TodoItemModel } from './todo-item-model';
 
 function TodoItem({ item, onDelete }: { item: TodoItemModel, onDelete: (todo: TodoItemModel) => void, }) {
 
-  return (<li className="min-w-[15rem] flex flex-row justify-between m-2">
-    <div>{item.description}</div>
-    <button className='font-bold text-red-600 p-1' onClick={() => onDelete(item)}>X</button>
+  return (<li className="min-w-[15rem] max-w-2xl w-[100dvw] flex flex-row justify-between px-[1.6rem] py-3">
+    <div className="text-lg">{item.description}</div>
+    <button className='font-bold text-red-600 py1 px-2 shadow-md hover:shadow-lg text-lg' onClick={() => onDelete(item)}>X</button>
   </li>)
 }
 
